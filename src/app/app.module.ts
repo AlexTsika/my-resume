31829bacf06f317e19a6c9eed48c54d894f99737
Alexandros Tsikakis <Alexandros.Tsikakis@ymail.com>
2023-04-08 20:33:31 +0200

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SkillsComponent } from './skills/skills.component';
+import { LanguagesComponent } from './languages/languages.component';
+import { EducationComponent } from './education/education.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ExperienceComponent,
+    ProfileComponent,
+    SkillsComponent,
+    LanguagesComponent,
+    EducationComponent,
+    FooterComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
